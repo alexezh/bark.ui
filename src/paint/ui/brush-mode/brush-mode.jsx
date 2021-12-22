bd325cb110a78b05f3cca5dc00445d7760d42058
Alexandre Grigorovitch <alexezh@gmail.com>
2021-12-21 22:58:59 -0800

    add more vector tools

--- src/paint/ui/brush-mode/brush-mode.jsx
+++ src/paint/ui/brush-mode/brush-mode.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 import messages from '../../lib/messages.js';
-import lineIcon from './line.svg';
+import brushIcon from './brush.svg';
 
-const LineModeComponent = props => (
+const BrushModeComponent = props => (
     <ToolSelectComponent
-        imgDescriptor={messages.line}
-        imgSrc={lineIcon}
+        imgDescriptor={messages.brush}
+        imgSrc={brushIcon}
         isSelected={props.isSelected}
         onMouseDown={props.onMouseDown}
     />
 );
 
-LineModeComponent.propTypes = {
+BrushModeComponent.propTypes = {
     isSelected: PropTypes.bool.isRequired,
     onMouseDown: PropTypes.func.isRequired
 };
 
-export default LineModeComponent;
+export default BrushModeComponent;
